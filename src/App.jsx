dc8cb3985a19423927989b6d435ed5edc56106a7
Hyusuka <juanrahawarin12@gmail.com>
2026-05-18 14:17:16 +0700

    remove: completely remove admin panel as requested

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import { PortfolioProvider } from './context/PortfolioContext';
 import Portfolio from './pages/Portfolio';
-import Admin from './pages/Admin';
 import './index.css';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <HashRouter>
         <Routes>
           <Route path="/" element={<Portfolio />} />
-          <Route path="/juan_wens_sanctung_rahawarin" element={<Admin />} />
         </Routes>
       </HashRouter> 
     </PortfolioProvider>
